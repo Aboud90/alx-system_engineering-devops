0f5cf91fdc67ac4087ceecfdc1512742d8fe9c33
Aboud90 <oudat.abdellatif@gmail.com>
2024-02-14 02:52:57 -0800

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,19 @@
 Queries the Reddit API and returns the number of total subscribers for a given
 subreddit.
 """
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
-    """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    url = f'https://api.reddit.com/r/{subreddit}'
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
