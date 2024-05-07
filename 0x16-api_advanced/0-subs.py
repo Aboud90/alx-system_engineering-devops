c347d0bd283af64c265d1f8d11fbed0e530229d1
Aboud90 <oudat.abdellatif@gmail.com>
2024-05-07 12:48:29 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+number of subscribers for a given subreddit
+"""
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My-User-Agent"}
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for non-2xx status codes
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    except requests.RequestException:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
